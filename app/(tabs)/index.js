e42c70334056b0fefe69e4fa5e656d7aa904be6f
Anne Caroline <carolaragao94@gmail.com>
2024-07-19 11:01:22 -0300

    logo update

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,9 +1,25 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 
 export default function App() {
     return(
-        <View>
-            
+        <View style={styles.container}>
+            <Image
+                source={require("../../src/assets/logo.png")}
+                style={styles.logo}
+            />
+            <Text style={styles.title}>Enter 20 characters</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: "#F3F3FF",
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    logo:{
+        marginBottom: 20
+    }
+})
