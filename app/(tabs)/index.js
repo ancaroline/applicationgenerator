5ca4af69908878d86782931a93bd3213988ca37f
Anne Caroline <carolaragao94@gmail.com>
2024-07-23 15:38:32 -0300

    useStorage

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,93 +1,10 @@
-import { userState, useState } from 'react'
-import Slider from '@react-native-community/slider'
-import { View, Text, StyleSheet, Image, TouchableOpacity, Modal } from 'react-native'
-import { ModalPassword } from '../../src/components/modal'
-
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+import { NavigationContainer } from '@react-navigation/native'
+import { Routes } from '../../src/routes'
 
 export default function App() {
-    const [size, setsize] = useState(10)
-    const [passwordValue, setPasswordValue] = useState("")
-    const [modalVisible, setModalVisible] = useState(false);
-
-    function generatePassword(){
-        let Password = "";
-        for(let i = 0, n = charset.length; i < size; i++){
-            Password += charset.charAt(Math.floor(Math.random() * n))
-        }
-
-        setPasswordValue(Password)
-        setModalVisible(true);
-        
-    }
-
     return(
-        <View style={styles.container}>
-            <Image
-                source={require("../../src/assets/logo.png")}
-                style={styles.logo}
-            />
-            <Text style={styles.title}>{size} characters</Text>
-
-            <View style={styles.area}>
-                <Slider
-                    style={{ height: 50 }}
-                    minimumValue={6}
-                    maximumValue={20}
-                    maximumTrackTintColor="#FF0000"
-                    minimumTrackTintColor="#000"
-                    thumbTintColor='grey'
-                    value={size}
-                    onValueChange={ (value) => setsize(value.toFixed(0)) }
-                />
-            </View>
-
-            <TouchableOpacity style={styles.button} onPress={generatePassword}>
-                <Text style={styles.buttonText}>Generate Password</Text>
-            </TouchableOpacity>
-
-            <Modal visible={modalVisible} animationType="fade" transparent={true}>
-                <ModalPassword password={passwordValue} handleClose={ () => setModalVisible(false) } />
-            </Modal>
-
-        </View>
+        <NavigationContainer independent={true}>
+            <Routes/>
+        </NavigationContainer>
     )
 }
-
-const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor: "#F3F3FF",
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    logo:{
-        marginBottom: 20
-    },
-    area:{
-        marginTop: 14,
-        marginBottom: 14,
-        width: "80%",
-        backgroundColor: "#FFF",
-        borderRadius: 8,
-        padding: 8,
-    },
-    button:{
-        backgroundColor: '#00AB71',
-        width: "80%",
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 8,
-        marginBottom: 18,
-    },
-    buttonText:{
-        color: "#FFF",
-        fontSize: 16,
-    },
-    title:{
-        fontSize: 25,
-        fontWeight: 'bold',
-
-    }
-})
